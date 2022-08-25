8cc95179c51fb48a2bbb0ac7faf344da5fb65fc8
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-08-26 02:38:02 +0600

    latest products section done

---
+++ components/Global/BasicProductItem.js
@@ -0,0 +1,126 @@
+import { Box, Rating, Stack, Typography } from '@mui/material';
+import React from 'react';
+import Image from 'next/image';
+import {RiHeartAddLine, RiAlignLeft, RiShoppingCart2Line} from 'react-icons/ri';
+import {HiSearch} from 'react-icons/hi';
+
+const BasicProductItem = ({ product }) => {
+    const { name, price, rating_star, image } = product;
+    return (
+        <Box
+        className='product_card_container'
+        sx={{
+            padding: '0px 0px',
+            borderTop: '1px solid #f0f2f5',
+            borderRight: '1px solid #f0f2f5',
+            borderBottom: '1px solid #f0f2f5'
+        }}>
+            <Box sx={{
+                textAlign: 'center',
+                padding: '30px 30px 0px 30px',
+                position: 'relative'
+            }}>
+                <Box>
+                    <Image
+                        src={image}
+                        alt="product image"
+                        width={150}
+                        height={150}
+                    />
+                </Box>
+                <Box className='product_card_overlay'>
+                    <Stack spacing={1}>
+                        <Box className="product_item_hover_element" sx={{
+                            width: '30px',
+                            height: '30px',
+                            background: '#f0f2f5',
+                            borderRadius: '50%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            cursor: 'pointer'
+                        }}>
+                            <RiHeartAddLine style={{
+                                fontSize: '16px',
+                                color: '#103178',
+                                fontWeight: '700'
+                            }} />
+                        </Box>
+                        <Box className="product_item_hover_element" sx={{
+                            width: '30px',
+                            height: '30px',
+                            background: '#f0f2f5',
+                            borderRadius: '50%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            cursor: 'pointer'
+                        }}>
+                            <RiAlignLeft style={{
+                                fontSize: '16px',
+                                color: '#103178',
+                                fontWeight: '700'
+                            }} />
+                        </Box>
+                        <Box className="product_item_hover_element" sx={{
+                            width: '30px',
+                            height: '30px',
+                            background: '#f0f2f5',
+                            borderRadius: '50%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            cursor: 'pointer'
+                        }}>
+                            <HiSearch style={{
+                                fontSize: '16px',
+                                color: '#103178',
+                                fontWeight: '700'
+                            }} />
+                        </Box>
+                        <Box className="product_item_hover_element" sx={{
+                            width: '30px',
+                            height: '30px',
+                            background: '#f0f2f5',
+                            borderRadius: '50%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            cursor: 'pointer'
+                        }}>
+                            <RiShoppingCart2Line style={{
+                                fontSize: '16px',
+                                color: '#103178',
+                                fontWeight: '700'
+                            }} />
+                        </Box>
+                    </Stack>
+                </Box>
+            </Box>
+            <Box sx={{
+                marginTop: '15px',
+                padding: '0px 30px 30px 30px'
+            }}>
+                <Typography sx={{
+                    color: '#103178',
+                    fontSize: '16px',
+                    fontWeight: '500',
+                    marginBottom: '15px'
+                }} variant='h5'>
+                    {name}
+                </Typography>
+                <Typography sx={{
+                    color: '#103178',
+                    fontSize: '18px',
+                    fontWeight: '600',
+                    marginBottom: '5px'
+                }} variant='h4'>
+                    ${price}
+                </Typography>
+                <Rating name="read-only" value={parseFloat(rating_star)} size="small" readOnly />
+            </Box>
+        </Box>
+    );
+};
+
+export default BasicProductItem;
--- components/Pages/Home/LatestProducts.js
+++ components/Pages/Home/LatestProducts.js
@@ -1,9 +1,75 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
 import ResponsiveContainer from '../../Global/ResponsiveContainer';
 import SectionTitle from '../../Global/SectionTitle';
+import demoProductImage from '../../../public/images/productDemoImage.png';
+import BasicProductItem from '../../Global/BasicProductItem';
+import { Navigation, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const LatestProducts = () => {
+    const products = [
+        {
+            name: "Lorem imsum dolor",
+            price: "30",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "50",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "40",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "60",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "20",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "30",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "30",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "30",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "50",
+            rating_star: "5",
+            image: demoProductImage
+        },
+        {
+            name: "Lorem imsum dolor",
+            price: "40",
+            rating_star: "5",
+            image: demoProductImage
+        },
+    ]
     return (
         <Box sx={{
             padding: '60px 0px'
@@ -18,7 +84,39 @@ const LatestProducts = () => {
                         </SectionTitle>
                     </Box>
                     <Box>
-
+                        <Box>
+                            <Box className='basic_product_grid' sx={{
+                                borderLeft: '1px solid #f0f2f5'
+                            }}>
+                                <Swiper
+                                    modules={[Navigation, A11y, Autoplay]}
+                                    spaceBetween={0}
+                                    speed={1000}
+                                    slidesPerView={5}
+                                    navigation={true}
+                                    breakpoints={{
+                                        1200: {
+                                            slidesPerView: 5,
+                                        },
+                                        900: {
+                                            slidesPerView: 3,
+                                        },
+                                        600: {
+                                            slidesPerView: 2,
+                                        },
+                                        320: {
+                                            slidesPerView: 1,
+                                        }
+                                    }}
+                                >
+                                    {
+                                        products.map((item, index) => <SwiperSlide key={index}>
+                                            <BasicProductItem  product={item} />
+                                        </SwiperSlide>)
+                                    }
+                                </Swiper>
+                            </Box>
+                        </Box>
                     </Box>
                 </Box>
             </ResponsiveContainer>
