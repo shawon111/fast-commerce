27dca1e1efa6173b1467e4181a15126056271777
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-08-27 18:43:18 +0600

    shipping features section complete

--- components/Global/ShippingFeatures.js
+++ components/Global/ShippingFeatures.js
@@ -1,10 +1,92 @@
+import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
+import ResponsiveContainer from './ResponsiveContainer';
+import { RiWalletLine, RiTruckLine } from 'react-icons/ri';
+import { BiShoppingBag } from 'react-icons/bi';
 
 const ShippingFeatures = () => {
     return (
-        <div>
-            
-        </div>
+        <Box sx={{
+            padding: '60px 0px 0px 0px'
+        }}>
+            <Box sx={{
+                borderTop: '1px solid #f0f2f5'
+            }}>
+                <ResponsiveContainer>
+                    <Box>
+                        <Grid container spacing={0}>
+                            <Grid sx={{ 
+                                borderRight: {sm: '1px solid #f0f2f5'},
+                            }} sm={4} xs={12} item>
+                                <Box sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    padding: '15px 0px',
+                                    justifyContent: 'center'
+                                }}>
+                                    <RiWalletLine style={{
+                                        fontSize: '18px',
+                                        color: '#103178',
+                                        marginRight: '10px'
+                                    }} />
+                                    <Typography sx={{
+                                        fontSize: '16px',
+                                        color: '#103178',
+                                        fontWeight: '600'
+                                    }} variant="h4">
+                                        100% Money back
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid sx={{
+                                borderRight: {sm: '1px solid #f0f2f5'},
+                            }} sm={4} xs={12} item>
+                                <Box sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    padding: '15px 0px',
+                                    justifyContent: 'center'
+                                }}>
+                                    <BiShoppingBag style={{
+                                        fontSize: '18px',
+                                        color: '#103178',
+                                        marginRight: '10px'
+                                    }} />
+                                    <Typography sx={{
+                                        fontSize: '16px',
+                                        color: '#103178',
+                                        fontWeight: '600'
+                                    }} variant="h4">
+                                        Non-contact shipping
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid sm={4} xs={12} item>
+                                <Box sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    padding: '15px 0px',
+                                    justifyContent: 'center'
+                                }}>
+                                    <RiTruckLine style={{
+                                        fontSize: '18px',
+                                        color: '#103178',
+                                        marginRight: '10px'
+                                    }} />
+                                    <Typography sx={{
+                                        fontSize: '16px',
+                                        color: '#103178',
+                                        fontWeight: '600'
+                                    }} variant="h4">
+                                        Free delivery for order over $200
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </ResponsiveContainer>
+            </Box>
+        </Box>
     );
 };
 
