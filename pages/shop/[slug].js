9030f583a79103c6b4dacdec98d368ec13816013
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-09-13 01:21:43 +0600

    data fetch function changed

--- pages/shop/[slug].js
+++ pages/shop/[slug].js
@@ -1,31 +1,56 @@
 import { Box, Grid } from '@mui/material';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '../../components/Global/Layout';
 import ResponsiveContainer from '../../components/Global/ResponsiveContainer';
 import CustomerAlsoBuy from '../../components/Pages/Shop/CustomerAlsoBuy';
 import SingleProductBody from '../../components/Pages/Shop/SingleProductBody';
 import SingleProductSidebar from '../../components/Pages/Shop/SingleProductSidebar';
+import { Bars } from 'react-loader-spinner';
 
-const Product = ({product, peopleAlsoBuyData, relatedProducts}) => {
+const Product = ({ product, peopleAlsoBuyData, relatedProducts }) => {
     const metaInfo = { title: `${product.metaTitle} | Fast Commerce`, keywords: product.metaTags, metaDesc: product.metaDescription };
+    const [showPreloader, setShowPreloader] = useState(true);
+
+    useEffect(() => {
+        setShowPreloader(false)
+    }, [product])
     return (
         <Box>
             <Layout metaInfo={metaInfo}>
                 <ResponsiveContainer>
-                    <Box sx={{
-                        marginTop: '50px'
-                    }}>
-                        <Grid container spacing={3}>
-                            <Grid lg={10} xs={12} item>
-                                <SingleProductBody product={product} />
-                            </Grid>
-                            <Grid sx={{
-                                display: {lg: 'block', xs: 'none'}
-                            }} lg={2} xs={12} item>
-                                <SingleProductSidebar relatedProducts={relatedProducts} />
-                            </Grid>
-                        </Grid>
-                    </Box>
+                    {
+                        showPreloader ?
+                            <Box sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                marginTop: { lg: '50px', sm: '30px', xs: '20px' }
+                            }}>
+                                <Bars
+                                    height="100"
+                                    width="100"
+                                    color="#103178"
+                                    ariaLabel="bars-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                    visible={true}
+                                />
+                            </Box> :
+                            <Box sx={{
+                                marginTop: '50px'
+                            }}>
+                                <Grid container spacing={3}>
+                                    <Grid lg={10} xs={12} item>
+                                        <SingleProductBody product={product} />
+                                    </Grid>
+                                    <Grid sx={{
+                                        display: { lg: 'block', xs: 'none' }
+                                    }} lg={2} xs={12} item>
+                                        <SingleProductSidebar relatedProducts={relatedProducts} />
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                    }
                 </ResponsiveContainer>
                 <CustomerAlsoBuy peopleAlsoBuyData={peopleAlsoBuyData} />
             </Layout>
@@ -35,30 +60,12 @@ const Product = ({product, peopleAlsoBuyData, relatedProducts}) => {
 
 export default Product;
 
-export const getStaticPaths = async () =>{
-    const res = await fetch(`https://fast-commerce-backend.onrender.com/products`)
-    const data = await res.json();
-
-    const paths = data.map(product=> {
-        return {
-            params: {
-                slug: `${product._id}`
-            }
-        }
-    })
-
-    return {
-        paths,
-        fallback: false
-    }
-}
-
-export const getStaticProps = async ({params}) =>{
+export const getServerSideProps = async ({ params }) => {
     const { slug } = params;
     const res = await fetch(`https://fast-commerce-backend.onrender.com/products/${slug}`);
     const data = await res.json();
 
-    const relatedRes =await fetch(`https://fast-commerce-backend.onrender.com/products/related/${data.category}`);
+    const relatedRes = await fetch(`https://fast-commerce-backend.onrender.com/products/related/${data.category}`);
     const relatedProducts = await relatedRes.json();
 
     const peopleBuyRes = await fetch("https://fast-commerce-backend.onrender.com/products/latest");
--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,49 +1,75 @@
 import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../../components/Global/Layout';
 import ResponsiveContainer from '../../components/Global/ResponsiveContainer';
 import ShopBody from '../../components/Pages/Shop/ShopBody';
 import ShopSidebar from '../../components/Pages/Shop/ShopSidebar';
+import { Bars } from 'react-loader-spinner';
 
-const Shop = ({watch, Products, productLength}) => {
+const Shop = ({ watch, Products, productLength }) => {
     const metaInfo = { title: "Shop | FastComerce | Best fashion store online", keywords: "fast commerce, ecommerce, shop, fast commerce shop", metaDesc: "Contact with fast commerce to get the best deal" };
 
     const [pageProduct, setPageProduct] = useState(Products);
     const [productPage, setProductPage] = useState(1);
+    const [showPreloader, setShowPreloader] = useState(true);
+
+    useEffect(() => {
+        setShowPreloader(false)
+    }, [Products])
 
     return (
         <>
             <Layout metaInfo={metaInfo}>
                 <ResponsiveContainer>
-                    <Box sx={{
-                        marginBottom: {lg: '40px', sm: '35px', xs: '28px'}
-                    }}>
-                        <Typography sx={{
-                            textAlign: 'start',
-                            fontSize: { lg: '42px', sm: '37px', xs: '33px' },
-                            color: '#103178',
-                            fontWeight: '700',
-                            marginTop: { lg: '40px', sm: '20px', xs: '20px' }
-                        }} variant="h1">
-                            Shop <sup style={{
-                                fontSize: '.5em',
-                                fontWeight: '300'
-                            }}>({productLength})</sup>
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Grid container spacing={3}>
-                            <Grid sx={{
-                                display: {lg: 'block', xs: 'none'}
-                            }} item lg={2}>
-                                <ShopSidebar />
-                            </Grid>
-                            <Grid item lg={10} xs={12}>
-                                <ShopBody setPageProduct={setPageProduct} setProductPage={setProductPage} Products={pageProduct} productLength={productLength} bestSeller={watch} />
-                            </Grid>
-                        </Grid>
-                    </Box>
+                    {
+                        showPreloader ?
+                            <Box sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                marginTop: { lg: '50px', sm: '30px', xs: '20px' }
+                            }}>
+                                <Bars
+                                    height="100"
+                                    width="100"
+                                    color="#103178"
+                                    ariaLabel="bars-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                    visible={true}
+                                />
+                            </Box> :
+                            <Box>
+                                <Box sx={{
+                                    marginBottom: { lg: '40px', sm: '35px', xs: '28px' }
+                                }}>
+                                    <Typography sx={{
+                                        textAlign: 'start',
+                                        fontSize: { lg: '42px', sm: '37px', xs: '33px' },
+                                        color: '#103178',
+                                        fontWeight: '700',
+                                        marginTop: { lg: '40px', sm: '20px', xs: '20px' }
+                                    }} variant="h1">
+                                        Shop <sup style={{
+                                            fontSize: '.5em',
+                                            fontWeight: '300'
+                                        }}>({productLength})</sup>
+                                    </Typography>
+                                </Box>
+                                <Box>
+                                    <Grid container spacing={3}>
+                                        <Grid sx={{
+                                            display: { lg: 'block', xs: 'none' }
+                                        }} item lg={2}>
+                                            <ShopSidebar />
+                                        </Grid>
+                                        <Grid item lg={10} xs={12}>
+                                            <ShopBody setPageProduct={setPageProduct} setProductPage={setProductPage} Products={pageProduct} productLength={productLength} bestSeller={watch} />
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </Box>
+                    }
                 </ResponsiveContainer>
             </Layout>
         </>
@@ -52,7 +78,7 @@ const Shop = ({watch, Products, productLength}) => {
 
 export default Shop;
 
-export async function getStaticProps(){
+export async function getServerSideProps() {
 
     const watchRes = await fetch(`https://fast-commerce-backend.onrender.com/products/category/watch`);
     const watch = await watchRes.json();
@@ -63,7 +89,7 @@ export async function getStaticProps(){
     const productLengthRes = await fetch(`https://fast-commerce-backend.onrender.com/products/length`);
     const productLength = await productLengthRes.json();
 
-    
+
     return {
         props: {
             watch,
